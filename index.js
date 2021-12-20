e51924e4881cd2839917110ea9f665cf2bb8a0ed
unknown <alexj.sabel@gmail.com>
2021-12-20 14:19:11 -0600

    added models, constructors, css, and html generation. Set up test files just need to make tests for each model

--- index.js
+++ index.js
@@ -1,86 +1,228 @@
+const generateWebPage = require('./src/generateWebPage.js');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const fs = require('fs');
 const inquirer = require('inquirer');
 
+// Empty array to store all team members
+let teamMembers = []
 
-class Employee {
-    constructor() {
-       this.name
-       this.id
-       this.email
-       this.getName()
-       this.getId()
-       this.getEmail()
-       this.getRole() 
-       this.Manager()
-    };
-
-      promptUser = () => {
-        return inquirer.prompt([
-          {
+// Questions for Managers
+const managerQuestions = () => {
+    console.log(`Team Manager Information`);
+    inquirer.prompt([
+    // Manager Office Number
+        {
             type: 'input',
-            name: 'Manager Name',
-            message: 'Manager Name? (Required)',
+            name: 'officeNumber',
+            message: "What is the Manager's office number?",
             validate: nameInput => {
-              if (nameInput) {
-                return true;
-              } else {
-                console.log('Please enter a name!');
-                return false;
-              }
+                if (isNaN(nameInput)) {
+                    console.log('Please enter an office number!');
+                    return false;
+                } else {
+                    return true;
+                }
             }
-          },
-          {
-            type: 'input',
-            name: 'Employee ID',
-            message: 'Enter their Employee ID (Required)',
-            validate: employeeID => {
-              if (employeeID) {
-                return true;
-              } else {
-                console.log('Please enter their Employee ID username!');
-                return false;
-              }
-            }
-          },
-          {
-            type: 'input',
-            name: 'Email',
-            message: 'Enter their Email (Required)',
-            validate: email => {
-              if (email) {
-                return true;
-              } else {
-                console.log('Please enter their Email Address!');
-                return false;
-              }
-            }
-          },
-          {
-            type: 'input',
-            name: 'OfficeNumber',
-            message: 'Enter their Office Number (Required)',
-            validate: officeNumb => {
-              if (officeNumb) {
-                return true;
-              } else {
-                console.log('Please enter their Office Number!');
-                return false;
-              }
-            }
-          },
-          {
+        }
+    ])
+    .then(managerData => {
+        let { officeNumber } = managerData;
+        let role = 'Manager';
+        employeeQuestions(role, officeNumber);
+    })
+};
+
+// Type of Employee you want to create
+const employeeTypeQuestions = () => {
+    inquirer.prompt([
+    // Employee type
+        {
             type: 'list',
-            message: 'Would you like to add an Engineer, a Intern, or finish your team?',
-            name: 'menu',
-            choices: ['Engineer', 'Intern', 'Finish Team']
-          }
-        ]);
+            name: 'role',
+            message: "Which type would you like to add?",
+            choices: ['Engineer', 'Intern']
+        }
+    ])
+    .then(empTypeData => {
+        let { role } = empTypeData;
         
-
-
-      };
-
+        if (role === 'Engineer') {
+            console.log('Engineer was selected! ', role)
+            engineerQuestions();
+        } else if (role === 'Intern') {
+            console.log('Intern was selected! ', role)
+            internQuestions();
+        } else {
+            console.log('How did you get here? (employeeTypeQuestions() Function)');
+        }
+    })
 }
 
+// Questions for Engineers
+const engineerQuestions = () => {
+    console.log(`Engineer Information`);
+    inquirer.prompt([
+    // Engineer GitHub Username
+        {
+            type: 'input',
+            name: 'ghUsername',
+            message: "Engineer's GitHub username:",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter Github');
+                }
+            }
+        }
+    ])
+    .then(engineerData => {
+        let { ghUsername } = engineerData;
+        let role = 'Engineer';
+        employeeQuestions(role, ghUsername);
+    })
+};
 
+// Questions for Interns
+const internQuestions = () => {
+    console.log(`Intern Information`);
+    inquirer.prompt([
+    // Intern School Name
+        {
+            type: 'input',
+            name: 'school',
+            message: "Intern's school?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter Employees school!');
+                }
+            }
+        }
+    ])
+    .then(internData => {
+        let { school } = internData;
+        let role = 'Intern';
+        employeeQuestions(role, school);
+    })
+};
 
-console.log('Connected')
+// Generic Questions for all types of employees
+const employeeQuestions = (value1, value2) => {
+    inquirer.prompt([
+    // Employee Name
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the their full name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Enter this employees full name');
+                }
+            }
+        },
+        // Employee ID #
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is their unique ID number?",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log('Enter this employee ID number');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }, 
+        // Employee Email Address
+        {
+            type: 'input', 
+            name: 'email',
+            message: "What is their email address?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Enter this employees email address');
+                }
+            }
+        }
+    ])
+    .then(employeeData => {
+        // Creates new Manager, Engineer, Intern based on role
+        if (value1 === 'Manager') {
+            let officeNum = value2;
+            let {name, id, email } = employeeData;
+            const manager = new Manager (name, id, email, officeNum);
+            teamMembers.push(manager);
+        } else if (value1 === 'Engineer') {
+            let ghUsername = value2;
+            let {name, id, email } = employeeData;
+            const engineer = new Engineer (name, id, email, ghUsername);
+            teamMembers.push(engineer);
+        } else if (value1 = 'Intern') {
+            let school = value2;
+            let {name, id, email } = employeeData;
+            const intern = new Intern (name, id, email, school);
+            teamMembers.push(intern);
+        } else {
+            console.log("How did you get here? (employeeQuestions constructor function)")
+        }
+        anotherEmployeeQuestions();
+    })
+};
+
+// Check to see if user wants to creat another employee, if not returns write to file function
+const anotherEmployeeQuestions = () => {
+    inquirer.prompt([
+    // Create another employee?
+        {
+            type: 'confirm',
+            name: 'addEmployeeResponse',
+            message: "Would you like to create another Team Member's profile?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+        }
+    ])
+    .then(employeeAddData => {
+        let { addEmployeeResponse } = employeeAddData;
+        if (addEmployeeResponse === true) {
+            employeeTypeQuestions();
+        } else {
+            console.log(`Team completed`);
+            return writeToFile(teamMembers);
+        }
+    })
+};
+
+function writeToFile(teamMembers) {
+
+    const content = generateWebPage(teamMembers);
+    fs.writeFile('./dist/index.html', content, err => {
+        if (err) {
+            console.error(err)
+            return
+        };
+        console.log(`Check dist folder for new index.html`)
+    })
+}
+
+// initiator function
+function init() {
+    managerQuestions()
+}
+
+init();
---
+++ src/generatePage.js
@@ -0,0 +1,104 @@
+function createEmployee(name, id, email, value1, value2) {
+    return `
+    <div class="col employee">
+        <div class="name-role">
+            <h3>${name}</h3>
+            ${value2}
+        </div>
+        <div class="employee-info">
+            <div class="info-box">
+                <p>ID: ${id}</p>
+            </div>
+            <div class="info-box">
+                <p>Email: <a href="mailto:${email}">${email}</a></p>
+            </div>
+            <div class="info-box">
+                ${value1}
+            </div>
+        </div>
+    </div>
+`
+}
+
+function createHTML(employeeSections) {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Team Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <header class="container-fluid my-team">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1><span class="align-middle">My Team</span></h1>
+            </div>
+        </div>
+    </header>
+    <main class="container-fluid mt-2">
+        <section class="row row-cols-3 employee-list">
+        ${employeeSections}
+        </section>
+    </main>
+    
+</body>
+</html>
+        `
+}
+
+
+const generateWebPage = function(data) {
+    // Create array to store what needs to be created in html
+    let teamMembersFull = [];
+
+    // iterate through and create each applicable employee
+    for (let i = 0; i < data.length; i++) {
+        let employee = data[i];
+
+        // obtain all info generic to all employees
+        let name = employee.getName();
+        let id = employee.getId();
+        let email = employee.getEmail();
+        let employeeType = employee.getRole();
+
+        if (employeeType === 'Manager') {
+            let officeNum = employee.getOfficeNum();
+            let symbol = `<h3><i class="fas fa-mug-hot"></i> Manager</h3>`;
+            let officeNumP = `<p>Office Number: ${officeNum}</p>`
+            const managerSection = createEmployee(name, id, email, officeNumP, symbol);
+            teamMembersFull.push(managerSection);
+
+        } else if (employeeType === 'Engineer') {
+            let ghUsername = employee.getGithub();
+            let symbol = `<h3><i class="fas fa-glasses"></i> Engineer</h3>`;
+            let ghUsernameP = `<p>GitHub Username: <a href="https://github.com/${ghUsername}">${ghUsername}</a></p>`;
+            const engineerSection = createEmployee(name, id, email, ghUsernameP, symbol);
+            teamMembersFull.push(engineerSection);
+
+        } else if (employeeType === 'Intern') {
+            let school = employee.getSchool();
+            let symbol =`<h3><i class="fas fa-user-graduate"></i> Intern</h3>`;
+            let schoolP = `<p>School: ${school}</p>`;
+            const internSection = createEmployee(name, id, email, schoolP, symbol);
+            teamMembersFull.push(internSection);
+
+        } else {
+            console.log("How did you get here? (generateWebPage Function)");
+        }
+    }
+
+    // Bring all elements togather...
+    const teamSections = teamMembersFull.join('');
+    // ... to sent to the function to create the HTML
+    return createHTML(teamSections);
+
+}
+
+module.exports = generateWebPage;
